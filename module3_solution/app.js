9c4482e487e41070e3846050b1af42648ac1edd3
sarmad-sabih-10p <sam_sarmad@hotmail.com>
2016-09-30 17:03:44 +0500

    changes

--- module3_solution/app.js
+++ module3_solution/app.js
@@ -11,7 +11,14 @@
     thisController.searchTerm = "";
     thisController.found = [];
     thisController.search = function(){
+      // MenuSearchService.getMatchedMenuItems(thisController.searchTerm).then(function(result){
+      //   thisController.found = result;
+      // });
+      alert("this should be last");
       thisController.found = MenuSearchService.getMatchedMenuItems(thisController.searchTerm);
+    };
+    thisController.removeItem = function(index){
+      alert(index);
     };
   };
 
@@ -19,30 +26,61 @@
   function MenuSearchService($http){
     var thisService = this;
 
+    thisService.allMenuItems = [];
+    thisService.found = [];
+
     thisService.getMatchedMenuItems = function(searchTerm){
+      if(thisService.allMenuItems.length){
+        // debugger;
+        thisService.found = filter(searchTerm, thisService.allMenuItems);
+        // return Promise.resolve(thisService.found);
+        return thisService.found;
+      }
+      else{
+        // debugger;
+        thisService.getAllMenuItems().then(function(result){
+          thisService.found = filter(searchTerm, thisService.allMenuItems);
+          alert(thisService.found);
+          return thisService.found;
+        });
+      }  
+      
+    };
+
+    thisService.getAllMenuItems = function(){
+
       return $http({
         method: 'GET',
         url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
       }).then(function (result) {
-        // process result and only keep items that match
-        var foundItems = [];
-        for (var i = 0; i < result.data.menu_items.length; i++) {
-          if(result.data.menu_items[i].description.indexOf(searchTerm) > -1){
-            foundItems.push(result.data.menu_items[i]);
-          }
-        }
-
-        console.log("result", foundItems);
+        thisService.allMenuItems = result.data.menu_items;
 
         // return processed items
-        return foundItems;
+        return thisService.allMenuItems;
       });
+
     };
+
+  };
+
+  function filter(searchTerm, allMenuItems){
+    // process result and only keep items that match
+    var foundItems = []
+    for (var i = 0; i < allMenuItems.length; i++) {
+      if(allMenuItems[i].description.indexOf(searchTerm) > -1){
+        foundItems.push(allMenuItems[i]);
+      }
+    }
+    return foundItems;
   };
 
   function FoundItems(){
     var ddo = {
-      templateUrl: "found-items.html"
+      scope: {
+        found: '=arr',
+        onRemove: '&'
+      },
+      template: "<p ng-repeat='item in found'><b>#:</b>{{$index+1}}, <b>Name:</b> {{ item.name }}, <b>Description:</b> {{ item.description }}. <button ng-click='onRemove({index: $index});'>Remove</button></p>"
     }
 
     return ddo;
